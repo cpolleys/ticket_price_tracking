e14d55bc9d5dddd220efafba2a7028caaef0293f
Chris <christopher.polleys@tufts.edu>
2026-02-21 18:12:50 -0500

    separate python file for loading dynamic web browser

---
+++ open_stubhub.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright
+
+URL = "https://www.stubhub.com/"
+
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        page.goto(URL, timeout=60000)
+        page.wait_for_timeout(10000)
+
+        browser.close()
+
+if __name__ == "__main__":
+    main()
