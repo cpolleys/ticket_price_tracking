fa8ae624e1f3f123d1968d73e03a49df1af5b372
Chris <christopher.polleys@tufts.edu>
2026-02-28 16:10:32 -0500

    edits to try and access the proper json information

--- .ipynb_checkpoints/scrape_stubhub_redsox-checkpoint.py
+++ .ipynb_checkpoints/scrape_stubhub_redsox-checkpoint.py
@@ -4,10 +4,13 @@ from playwright.async_api import async_playwright
 url = 'https://www.stubhub.com/boston-red-sox-boston-tickets-8-22-2026/event/159260973/?backUrl=%2Fboston-red-sox-tickets%2Fperformer%2F4322&quantity=2'
 
 async def test_json(response):
-    try:
-        print(await response.json())
-    except:
-        pass
+    if request.resource_type == "fetch":
+        print("REQUEST:", request.url)
+        #print("XHR:", response.url)
+    #try:
+        #print(await response.json())
+    #except:
+        #pass
 
 async def scrape_stubhub_redsox():
     async with async_playwright() as p:
@@ -16,6 +19,8 @@ async def scrape_stubhub_redsox():
 
         page.on('response', lambda response: test_json(response))
         await page.goto(url)
+        await page.mouse.wheel(0, 4000)
+        await page.wait_for_timeout(8000)
         
         
         
